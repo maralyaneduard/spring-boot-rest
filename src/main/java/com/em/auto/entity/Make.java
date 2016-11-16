8c4da3daff612a8b24c0f03b94463927d29e0f6e
eduardmaralyan <maralyaneduard@github.com>
2016-11-16 15:37:37 +0400

    add make and model antity classes

---
+++ src/main/java/com/em/auto/entity/Make.java
@@ -0,0 +1,47 @@
+package com.em.auto.entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by eduardm on 11/16/16.
+ */
+@Entity(name = "Make")
+public class Make {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "make_id")
+    private Long makeId;
+    @Column(name = "make_name", unique = true)
+    private String makeName;
+
+    @OneToMany(mappedBy = "make", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<Model> models;
+
+    public Make() {
+    }
+
+    public Long getMakeId() {
+        return makeId;
+    }
+
+    public void setMakeId(Long makeId) {
+        this.makeId = makeId;
+    }
+
+    public String getMakeName() {
+        return makeName;
+    }
+
+    public void setMakeName(String makeName) {
+        this.makeName = makeName;
+    }
+
+    public Set<Model> getModels() {
+        return models;
+    }
+
+    public void setModels(Set<Model> models) {
+        this.models = models;
+    }
+}
---
+++ src/main/java/com/em/auto/entity/Model.java
@@ -0,0 +1,49 @@
+package com.em.auto.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by eduardm on 11/16/16.
+ */
+@Entity(name = "Model")
+public class Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "model_id")
+    private Long modelId;
+    @Column(name = "model_name", unique = true)
+    private String modelName;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "make_id")
+    private Make make;
+
+    public Model() {
+    }
+
+    public Long getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(Long modelId) {
+        this.modelId = modelId;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public Make getMake() {
+        return make;
+    }
+
+    public void setMake(Make make) {
+        this.make = make;
+    }
+}
+
